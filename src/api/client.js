180312605a5f4978d577f3c735f9d5cbe5782ebe
Sachin Rathod <scrathod921@gmail.com>
2026-01-16 17:39:49 +0530

    Update Backend URL

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8080/api/files";
+//const BASE_URL = "http://localhost:8080/api/files";
+const BASE_URL = "https://filetransfer-backend-eawf.onrender.com/api/files";
 
 
 const USERNAME = "bankuser";
